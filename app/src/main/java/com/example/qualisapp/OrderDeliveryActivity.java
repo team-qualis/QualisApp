dcb6c66870cde5a403fd07b365fdc865722ab6c9
Jingyu Su <suesusususu@gmail.com>
2019-04-03 15:32:52 -0400

    Made the zone selection menu.

--- app/src/main/java/com/example/qualisapp/OrderDeliveryActivity.java
+++ app/src/main/java/com/example/qualisapp/OrderDeliveryActivity.java
@@ -4,15 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class OrderDeliveryActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_delivery);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.zone_address, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+
+                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position)+" is selected", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){
+
+            }
+
+
+        });
 
         ImageButton returnBttn = (ImageButton) findViewById(R.id.returnBttn);
         returnBttn.setOnClickListener(new View.OnClickListener() {
